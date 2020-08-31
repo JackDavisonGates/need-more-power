379f36432c720cefcda85cf69bd0158f5f9fe3a5
Jack Davison-Gates <36479356+jack-york@users.noreply.github.com>
2020-08-31 23:06:08 +0100

    x

--- js/toolTips.js
+++ js/toolTips.js
@@ -2,11 +2,15 @@ function toolTips(id) {
     switch (id) {
         case "noTip":
             document.getElementById("tool_tip").innerHTML = ""
+            document.getElementById("flavour_text").innerHTML = ""
             break;
         case "generator":
             document.getElementById("tool_tip").innerHTML =
-                "this is a test of the tool tip \n
-                by clicking the generator it will speed up"
+                "This is a test of the tool tip \n
+                by clicking the generator it will speed up."
+            document.getElementById("flavour_text").innerHTML =
+                "Electricity is one of man's greatest inventions \n
+                it is up there with fire computers and twerking."
             break;
         default:
 
